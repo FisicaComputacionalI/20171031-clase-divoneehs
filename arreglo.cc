89cf32a9f94d561d04e7daec451eeed265eca25c
Diana Ivonee Huitzil Sosa <31254618+divoneehs@users.noreply.github.com>
2017-10-31 13:07:08 -0600

    Add files via upload
    Hola :) 
    adjunto el programa que hicimos en clase, 
    espero sea verdadero y termine a los 23 xD

---
+++ arreglo.cc
@@ -0,0 +1,20 @@
+//Diana Ivonee Huitzil Sosa
+//El programa da el promedio de edad a la que se graduara el grupo
+#include<iostream>
+using namespace std;
+int main(){
+  int sum=0;
+  int edad[16];
+  for(int i=0; i<16; i++){
+    cout<<"¿ Que edad tienes?" <<endl;
+    cin>>edad[i];
+  }
+  for(int j=0; j<16; j++)
+    sum+=(edad[j]+3);
+  for(int j=0; j<16; j++)
+    cout<<edad[j]<<" , " ;
+  cout<<endl;
+  cout<<"el promedio de edad a la que se graduara este grupo es " <<sum/16<<endl;
+  return 0;
+}
+ 
