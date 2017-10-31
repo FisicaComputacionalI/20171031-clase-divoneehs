aa8700e06e3e5e291fc1e48cda809e2e484b5e87
Diana Ivonee Huitzil Sosa <31254618+divoneehs@users.noreply.github.com>
2017-10-31 13:20:31 -0600

    Add files via upload
    Este es el archivo modificado para que el usuario elija el numero de alumnos encuestados.

--- arreglo.cc
+++ arreglo.cc
@@ -3,15 +3,18 @@
 #include<iostream>
 using namespace std;
 int main(){
+  size_t talla=0;
+  cout<<"¿Cuantos alumnos encuestaras ?" <<endl;
+  cin>>talla;
+  int*edad= new int [talla];
   int sum=0;
-  int edad[16];
-  for(int i=0; i<16; i++){
+  for(int i=0; i<talla; i++){
     cout<<"¿ Que edad tienes?" <<endl;
     cin>>edad[i];
   }
-  for(int j=0; j<16; j++)
+  for(int j=0; j<talla; j++)
     sum+=(edad[j]+3);
-  for(int j=0; j<16; j++)
+  for(int j=0; j<talla; j++)
     cout<<edad[j]<<" , " ;
   cout<<endl;
   cout<<"el promedio de edad a la que se graduara este grupo es " <<sum/16<<endl;
